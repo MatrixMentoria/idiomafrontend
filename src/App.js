6ee970cd49b059ac05d892666e1bbad6ff6f47db
Thiago Lemos <thiagolemos1996@gmail.com>
2018-10-15 22:33:04 -0300

    adição dos botões play e remover dos marcadores

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ import img3 from "./medias/hideicon.png";
 import img4 from "./medias/retornar3s.png";
 import img5 from "./medias/avancar3s.png";
 import img6 from "./medias/bookmark.png";
+import img7 from "./medias/play.png";
+import img8 from "./medias/excluir.png";
 
 class App extends Component {
   state = {
-
     //Info Player
     tempo: document.getElementsByTagName("audio"),
     velocidade: null,
@@ -33,69 +34,69 @@ class App extends Component {
     },
 
     //Infos Marcadores
-    marcadores: [],
-
+    marcadores: []
   };
 
   componentDidMount = () => {
-    axios.get("http://idiomabackend.herokuapp.com/audio/")
-      .then( result => {
-        const audioId = result.data[0].id;
-        const link = result.data[0].link;
-        const textoEN = result.data[0].englishTranscription;
-        const textoPT = result.data[0].portugueseTranscription;
-        const total = result.data[0].duration;
+    axios.get("http://idiomabackend.herokuapp.com/audio/").then(result => {
+      const audioId = result.data[0].id;
+      const link = result.data[0].link;
+      const textoEN = result.data[0].englishTranscription;
+      const textoPT = result.data[0].portugueseTranscription;
+      const total = result.data[0].duration;
 
-        this.setState({
-          audioId: audioId,
-          link: link,
-          textoEN: textoEN,
-          textoPT: textoPT,
-          duracao: {
-            total: total
-          },
-          velocidade: "1.0x",
-        });
-
-        this.converterSegundos(total);
-        this.popularListaMarcadores();
+      this.setState({
+        audioId: audioId,
+        link: link,
+        textoEN: textoEN,
+        textoPT: textoPT,
+        duracao: {
+          total: total
+        },
+        velocidade: "1.0x"
       });
 
-    axios.get("http://idiomabackend.herokuapp.com/user/1")
-      .then( result => {
-        const userId = result.data.id;
-        this.setState({
-          userId: userId
-        });
+      this.converterSegundos(total);
+      this.popularListaMarcadores();
+    });
+
+    axios.get("http://idiomabackend.herokuapp.com/user/1").then(result => {
+      const userId = result.data.id;
+      this.setState({
+        userId: userId
       });
-  }
+    });
+  };
 
   popularListaMarcadores = () => {
-    axios.get(`https://idiomabackend.herokuapp.com/marking?userId=1&audioId=1`)
-      .then( result => {
+    axios
+      .get(`https://idiomabackend.herokuapp.com/marking?userId=1&audioId=1`)
+      .then(result => {
         const marcadores = result.data;
         this.setState({
-          marcadores: marcadores,
+          marcadores: marcadores
         });
       });
-  }
+  };
 
   adicionarMarcador = novoMarcador => {
-    axios.post("http://idiomabackend.herokuapp.com/marking/", novoMarcador)
-      .then( result => {
+    axios
+      .post("http://idiomabackend.herokuapp.com/marking/", novoMarcador)
+      .then(result => {
         alert("Marcador nº " + result.data.id + " cadastrado com sucesso!");
       });
-  }
+  };
 
   excluirMarcarao = id => {
-    axios.delete("http://idiomabackend.herokuapp.com/marking/" + id)
-      .then( () => {
+    axios
+      .delete("http://idiomabackend.herokuapp.com/marking/" + id)
+      .then(() => {
         alert("Marcador nº " + id + " excluído com sucesso!");
       })
-      .then( () => {
+      .then(() => {
         window.location.reload(true);
       });
-  }
+  };
 
   gerarMarcacao = () => {
     const novoMarcador = {
@@ -103,14 +104,14 @@ class App extends Component {
       userId: this.state.userId,
       begin: this.state.tempo[0].currentTime - 1,
       end: this.state.tempo[0].currentTime + 3
-    }
+    };
     this.adicionarMarcador(novoMarcador);
   };
 
   tocarMarcacao = marcadorBegin => {
     this.state.tempo[0].currentTime = marcadorBegin;
     this.state.tempo[0].play();
-    setTimeout( () => {
+    setTimeout(() => {
       this.state.tempo[0].pause();
     }, 5000);
   };
@@ -130,23 +131,22 @@ class App extends Component {
   };
 
   alterarVelocidadeAudio = info => {
+    info === true
+      ? (this.state.tempo[0].playbackRate += 0.1)
+      : (this.state.tempo[0].playbackRate -= 0.1);
 
-    info === true ?
-      this.state.tempo[0].playbackRate += 0.1 :
-      this.state.tempo[0].playbackRate -= 0.1;
+    this.state.tempo[0].playbackRate > 2
+      ? (this.refs.bMais.disabled = true)
+      : (this.refs.bMais.disabled = false);
 
-    this.state.tempo[0].playbackRate > 2 ?
-      this.refs.bMais.disabled = true :
-      this.refs.bMais.disabled = false;
-
-    this.state.tempo[0].playbackRate < 0.51 ?
-      this.refs.bMenos.disabled = true :
-      this.refs.bMenos.disabled = false;
+    this.state.tempo[0].playbackRate < 0.51
+      ? (this.refs.bMenos.disabled = true)
+      : (this.refs.bMenos.disabled = false);
 
     const velocidade = this.state.tempo[0].playbackRate.toFixed(1).toString();
 
     this.setState({
-      velocidade: (velocidade + "x"),
+      velocidade: velocidade + "x"
     });
   };
 
@@ -172,7 +172,7 @@ class App extends Component {
         {this.MarcadoresLista()}
       </div>
     );
-  }
+  };
 
   Player = () => {
     return (
@@ -191,7 +191,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 
   ControlesPlayer = () => {
     return (
@@ -222,10 +222,13 @@ class App extends Component {
             type="button"
             className="btn btn-light btn-text"
             id="menos"
-            onClick={ () => { this.alterarVelocidadeAudio(false); }}
+            onClick={() => {
+              this.alterarVelocidadeAudio(false);
+            }}
             ref="bMenos"
           >
-            {" "}-{" "}
+            {" "}
+            -{" "}
           </button>
           <label
             type="button"
@@ -238,15 +241,18 @@ class App extends Component {
             type="button"
             className="btn btn-light btn-text"
             id="mais"
-            onClick={ () => { this.alterarVelocidadeAudio(true); }}
+            onClick={() => {
+              this.alterarVelocidadeAudio(true);
+            }}
             ref="bMais"
           >
-            {" "}+{" "}
+            {" "}
+            +{" "}
           </button>
         </div>
       </div>
     );
-  }
+  };
 
   CardUm = () => {
     return (
@@ -261,7 +267,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 
   Imagens_PT_EN_Null = () => {
     return (
@@ -279,7 +285,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 
   CardDois = () => {
     return (
@@ -294,7 +300,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 
   ControlesPagina = () => {
     return (
@@ -316,7 +322,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 
   MarcadoresLista = () => {
     return (
@@ -336,37 +342,37 @@ class App extends Component {
             <table style={{ marginTop: 10 }} className="table table-hover">
               <thead>
                 <tr>
-                  <th>Id</th>
                   <th>Início</th>
                   <th>Fim</th>
                   <th>Ações</th>
                 </tr>
               </thead>
               <tbody>
-                {this.state.marcadores.map( marcador => {
+                {this.state.marcadores.map(marcador => {
                   return (
                     <tr key={marcador.id}>
-                      <td>{marcador.id}</td>
                       <td>{marcador.begin}</td>
                       <td>{marcador.end}</td>
                       <td>
-                        <button
-                          className="btn btn-outline-primary"
-                          onClick={ () => {
+                      <input
+                          type="image"
+                          alt="img"
+                          src={img7}
+                          id="icon-play"
+                          onClick={() => {
                             this.tocarMarcacao(marcador.begin);
                           }}
-                        >
-                          Selecionar
-                        </button>
-                        {" "}
-                        <button
-                          className="btn btn-outline-primary"
-                          onClick={ () => {
+                        />
+                        {"    "}
+                        <input
+                          type="image"
+                          alt="img"
+                          src={img8}
+                          id="icon-excluir"
+                          onClick={() => {
                             this.excluirMarcarao(marcador.id);
                           }}
-                        >
-                          Excluir
-                        </button>
+                        />
                       </td>
                     </tr>
                   );
@@ -377,7 +383,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
