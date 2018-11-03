fef571bbc0d34612f51aa4a35b9781163eb42a9b
Daniel Yago Orfeu <daniel_yago1997@hotmail.com>
2018-11-03 00:33:27 -0300

    Refatoração de código.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./css/App.css";
-import "./css/styles.css";
 import Login from "./Login";
 import Player from "./Player";
 
 class App extends Component {
   render = () => {
     return (
-      <div>
-        <main>
-          <Switch>
-            <Route path="/Player" component={Player} />
-            <Route path="/App" component={Login} />
-            <Route path="/" component={Login} />
-          </Switch>
-        </main>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Player" component={Player} />
+      </Switch>
     );
   };
 }
